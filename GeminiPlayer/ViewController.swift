b33f282880a3ffa4a10a6d9893e87cb3ec2d656b
fxfyio <fxfyio@pm.me>
2023-05-21 01:59:45 +0800

    first commit

---
+++ GeminiPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeminiPlayer
+//
+//  Created by zc on 5/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
