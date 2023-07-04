bdc6a8bb23b58aee00ead865e3f84a8937cc347d
fxfyio <fxfyio@pm.me>
2023-07-04 16:53:53 +0800

    import ffmpeg 6.0

---
+++ GeminiPlayer/GeminiPlayer/GeminiPlayer-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavfilter/avfilter.h"
+#include "libavdevice/avdevice.h"
+#include "libswresample/swresample.h"
+#include "libswscale/swscale.h"
+#include "libavutil/avutil.h"
+#include "libavutil/opt.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/samplefmt.h"
--- GeminiPlayer/ViewController.swift
+++ GeminiPlayer/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let version = String(cString: av_version_info())
+        let config = String(cString: avcodec_configuration())
+        let license = String(cString: avcodec_license())
+        print("FFmpeg version: \(version)")
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.frame = self.view.bounds
+        label.textAlignment = .center
+        label.text = version + "\n" + config + "\n" + license
+        self.view.addSubview(label)
     }
 
 
